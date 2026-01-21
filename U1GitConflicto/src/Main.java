f8a668f36e41efaf88ac3def859427594e733fac
Leonardo Rodriguez <20253DS137@utez.edu.mx>
2026-01-21 08:09:55 -0600

    develop: cambio para conflicto

--- U1GitConflicto/src/Main.java
+++ U1GitConflicto/src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static final String VERSION_OBJETIVO = "PENDIENTE";
+
+    public static void main(String[] args) {
+        System.out.println("Version: " + VERSION_OBJETIVO);
+    }
 }
